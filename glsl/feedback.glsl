c28e82a247513cc39706e8593b8e14f01c31dfe3
julesb <julesb@users.noreply.github.com>
2021-07-04 11:02:46 +0930

    crt effect, hue rotation, brcosa, tweaks

--- glsl/feedback.glsl
+++ glsl/feedback.glsl
@@ -1,7 +1,7 @@
 
 // 0 = clip
 // 1 = mirror
-#define BOUNDARY_MODE 0
+#define BOUNDARY_MODE 1
 
 #define  PI 3.1415927
 #define TAU 6.2831853
@@ -13,8 +13,25 @@ uniform float time;
 uniform vec2 mouse;
 uniform float zoom;
 uniform float zrot;
+uniform float brightness;
+uniform float contrast;
+uniform float saturation;
 
 uniform sampler2D tex1;
+
+vec3 ContrastSaturationBrightness(vec3 color, float con, float sat, float brt) {
+    const float AvgLumR = 0.5;
+    const float AvgLumG = 0.5;
+    const float AvgLumB = 0.5;
+    const vec3 LumCoeff = vec3(0.2125, 0.7154, 0.0721);
+    vec3 AvgLumin  = vec3(AvgLumR, AvgLumG, AvgLumB);
+    vec3 brtColor  = color * brt;
+    vec3 intensity = vec3(dot(brtColor, LumCoeff));
+    vec3 satColor  = mix(intensity, brtColor, sat);
+    vec3 conColor  = mix(AvgLumin, satColor, con);
+    return conColor;
+}
+
 
 mat2 rot(float a) {
     float s=sin(a), c=cos(a);
@@ -83,6 +100,31 @@ vec3 blur(in vec2 p){
     return res/16.;
 }
 
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233)))* 43758.5453123);
+}
+
+
+vec3 crt(vec2 uv, vec3 col) {
+    float density = 2.0;
+    float opacityScanline = 0.5;
+    float opacityNoise = .5;
+    float flickering = 0.03;
+
+    float count = resolution.y * density;
+    vec2 sl = vec2(sin(uv.y * count), cos(uv.y * count)) * 0.5 + 0.5;
+    //sl = sl * 0.8 + 0.2;
+	vec3 scanlines = vec3(sl.x, sl.y, sl.x);
+
+    col += col * scanlines * opacityScanline;
+    col += col * vec3(random(uv*time)) * opacityNoise;
+    col += col * sin(110.0*time) * flickering;
+
+
+    return col;
+}
+
 void main(void) {
     float scale = 2.;
     vec2 uv = (vertTexCoord.st - 0.5) * vec2(aspect_ratio, 1.0);
@@ -95,37 +137,41 @@ void main(void) {
     // rotate / zoom previous frame UVs
     vec2 puv = (vertTexCoord.st - 0.5 ) * zoom * vec2(aspect_ratio, 1.0);
     
-    float ang = cos(time*0.005) * TAU;
+    float ang = cos(time*0.01) * PI * 0.5;
 
-    puv *= rot(ang); // auto rotate
     //puv *= rot(zrot); // interactive rotate
+    puv *= rot(zrot+ang); // auto rotate
     puv.x /= aspect_ratio;
     puv += 0.5;
     
     // screenspace wobble
-    //puv.x += cos(time*1. + uv.y*15.321) * 0.002125;
-    //puv.y += sin(time*1.1 + uv.x*15.) * 0.002125;
+    puv.x += cos(time*1. + uv.y*15.321) * 0.001125;
+    puv.y += sin(time*1.1 + uv.x*15.) * 0.001125;
 
     // auto mouse wander
-    m = vec2(cos(time*0.74 + uv.y*5.321) * 0.4,
-             sin(time*0.68 + uv.x*1.) * 0.4);
+    m = vec2(cos(time*0.54 + uv.y*5.321) * 0.3,
+             sin(time*0.38 + uv.x*1.) * 0.3);
 
     vec3 col = vec3(0.0);
 
-    vec3 dotcol = hsv2rgb(vec3(ang * 60., 1.0, 1.0));
     // dot at mouse
-   col += (dotcol * exp_glow(uv-m, 0.0025));
-    //col += hsv2rgb(vec3(time*0.2, 1., 1.)) * exp_glow(uv-m, 0.0015);
+    vec3 dotcol = hsv2rgb(vec3(fract(time * 0.01) , 1.0, 1.0));
+   col += (dotcol * exp_glow(uv-m, 0.01));
 
     // axis reference dot at x = +1
     //col += vec3(1,0,0) * vec3(exp_glow(uv*scale-vec2(1.0, 0.0), 0.0125));
     // axis reference dot at y = +1
     //col += vec3(0,1,0) * vec3(exp_glow(uv*scale-vec2(0.0, 1.0), 0.0125));
+
+    // rbg noise
+    //col += vec3(hash21(uv+time), hash21(uv*1.1+time), hash21(uv*1.2+time)) * 0.15;
+
+    col *= crt(puv, col);
     
     if (BOUNDARY_MODE == 0) {
         // clip puv
         if (puv.x > 0. && puv.x < 1. && puv.y > 0. && puv.y < 1.) {
-            //col += blur(puv).rgb;// * 0.95;
+            //col += blur(puv).rgb; // * 0.90;
             col += tx(puv).rgb;// * 0.9;
         }
     }
@@ -134,22 +180,27 @@ void main(void) {
         puv = abs(puv);
         if (puv.x > 1.) puv.x = 1. - puv.x;
         if (puv.y > 1.) puv.y = 1. - puv.y;
-        col += tx(puv).rgb * 0.95;
-
+        //col += tx(puv).rgb; // * 0.95;
+        col += blur(puv).rgb;
     }
 
-    //col = (col - 0.125) * 2.0;
 
-    //col = col*col;
+    // hue rotation
+    col = rgb2hsv(col);
+    col.x += 0.01;
+    //col.x += fract(time*0.001);
+    col = hsv2rgb(col);
 
-    // noise
-    //col += hash23(uv * time*0.1) * 0.1;
-    //col += hash21(uv+time) * 0.01;
-    //col = vec3(hash21(uv+time), hash21(uv*1.1+time), hash21(uv*1.2+time)) * 1.00;
-    
+    //col = vec3(1);
+
+
+    col = ContrastSaturationBrightness(col, contrast, saturation, brightness);
+
+    //col = col * col;
+    //col *= crt(uv, col);
     //col *= (0.97 + vec3(length(uv)*2.)*0.03); // vignette
-    //col *= (1.- dot(uv, uv)); // vignette
+    col *= (1.- dot(uv, uv)); // vignette
 
-    gl_FragColor = vec4(col, 1.0);
+    gl_FragColor = vec4(col, 0.75);
 }
 
