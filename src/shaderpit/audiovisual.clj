7c9550de4405583b39bb2bdf63d9be1b4d0290d2
julesb <julesb@users.noreply.github.com>
2021-07-31 14:58:52 +0930

    fft channel delta experiment

--- src/shaderpit/audiovisual.clj
+++ src/shaderpit/audiovisual.clj
@@ -4,8 +4,10 @@
 
 
 (defn draw-meter [x y w h rms]
-  (let [rmss (* rms h 6)
-        mh (min rmss h)
+  (let [
+        rmss (* (Math/sqrt rms) h)
+        ;rmss (* rms h 6)
+        mh rmss ;(min rmss h)
         my (- (+ y h) mh )
         col (if (> rmss h) [255 0 0 128] [0 255 0 128]) ]
     (q/no-fill)
@@ -36,6 +38,16 @@
     (q/stroke 128 128 128 128)
     (q/rect x y w h)
     (q/stroke-weight (max (/ 0.5 s) 2))
+
+    (q/stroke 0 0 255 255)
+    (q/begin-shape :points)
+    (doseq [i (range texsize)]
+      (let [idx (int (* i ))
+            x1 (aget px idx)
+            sy (- (/ (q/blue x1) 255.0) 0.0) ]
+        (q/vertex (+ x (/ i s))
+                  (- (+ y h) (* sy (- h 3))))))
+    (q/end-shape)
 
     (q/stroke 255 0 0 255)
     (q/begin-shape :points)
@@ -80,13 +92,17 @@
 
 
 (defn draw-beats [x y w]
+  (let [margin 20
+        w2 (- w margin)]
+  (q/stroke-weight 1.0)
+  (q/stroke 128 128 128 128)
   (q/fill 0 0 0 192)
-  (q/rect (- x (/ w 2)) (- y (/ w 2)) (* w 5) w (/ w 2))
+  (q/rect (- x (/ w 2)) (- y (/ w 2)) (* w 5) w (* w 2))
   (q/fill 255 0 0 (* (audio/get-kick) 255))
-  (q/ellipse (+ x (* 0 w)) y w w)
+  (q/ellipse (+ x (* 0 w)) y w2 w2)
   (q/fill 255 255 0 (* (audio/get-snare) 255))
-  (q/ellipse (+ x (* 2 w)) y w w)
+  (q/ellipse (+ x (* 2 w)) y w2 w2)
   (q/fill 0 0 255 (* (audio/get-hat) 255))
-  (q/ellipse (+ x (* 4 w)) y w w))
+  (q/ellipse (+ x (* 4 w)) y w2 w2)))
 
 
